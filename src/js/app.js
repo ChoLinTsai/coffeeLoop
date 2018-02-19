0fe43f3286fdb2edeffd387154b0292f119d2029
Cho Lin Tsai <max19851113@gmail.com>
2018-02-19 21:12:44 +0800

    header js still wip

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 const css = require('../css/app.scss');
 require("font-awesome-sass-loader");
-
+import coffeeShops from './shops';
 // short for console.log()
 function $log(x) {
   return console.log(x);
@@ -29,59 +29,27 @@ function $rmClsls(element, ...rmClass) {
   return element.classList.remove(...rmClass);
 }
 
-// get a#menu-btn and line 1, 2, 3
-const menuBtn = $select('#menu-btn'),
-  line1 = $select('#line-1'),
-  line2 = $select('#line-2'),
-  line3 = $select('#line-3'),
-  closeBtn = $select('#close-btn');
-
-// event mouse over/out on menuBtn
-$event(menuBtn, "mouseover", menuBtnMouseOver);
-$event(menuBtn, "mouseout", menuBtnMouseOut);
-
-// event click on menuBtn to open shop menu
-$event(menuBtn, "click", menuBtnClick);
-
-// click close btn to close shop menu
-$event(closeBtn, 'click', closeBtnClick);
-
-// function to rotate line 1 , 3  and hide line 2
-function menuBtnMouseOver() {
-  line1.style.transform = "rotate(20deg)";
-  line2.style.display = "none";
-  line3.style.transform = "rotate(-20deg)";
+// make a 2d array
+function get2dArray(x, y) {
+  let resultArray = [];
+  for (let i = 0; i < x.length; i++) {
+    resultArray.push([
+      x[i], y[i]
+    ]);
+  }
+  return resultArray;
 }
 
-// function to rotate back line 1 , 3 and show line 2
-function menuBtnMouseOut() {
-  line1.style.transform = "rotate(0deg)";
-  line2.style.display = "block";
-  line3.style.transform = "rotate(0deg)";
-}
+$event(window, 'DOMContentLoaded', windowOnLoad, false);
 
-// function click to move menuBtn
-function menuBtnClick() {
-  let divBg = $select('.bg'),
-    menuLines = [...$selectAll('.menu-line')],
-    headerh1 = [...$selectAll('.header-h1')],
-    headerContent1 = $select('#header-content-1'),
-    headerContent2 = $select('#header-content-2');
-
-  divBg.style.animation = "bgFill 0.5s";
-  menuLines.map(i => i.style.visibility = 'hidden');
-  headerContent1.style.transform = 'translateX(100%)';
-  headerContent2.style.transform = 'translateX(0)';
-}
-
-function closeBtnClick() {
-  let divBg = $select('.bg'),
-    menuLines = [...$selectAll('.menu-line')],
-    headerContent1 = $select('#header-content-1'),
-    headerContent2 = $select('#header-content-2');
-
-  divBg.style.animation = "bgUnFill 0.5s";
-  menuLines.map(i => i.style.visibility = 'visible');
-  headerContent2.style.transform = 'translateX(-100%)';
-  headerContent1.style.transform = 'translateX(0%)';
+function windowOnLoad() {
+  // set liShopNames as array of .li-shop-name
+  let liShopNames = [...$selectAll('.li-shop-name')];
+  // get liShopNames and coffeeShops into 2d array
+  let getCoffeeShopNames = get2dArray(liShopNames, coffeeShops)
+  // map out all cafe names into liShopNames
+  getCoffeeShopNames.map(i => {
+    let [x, y] = i;
+    x.innerHTML = y.name;
+  })
 }
---
+++ src/js/shops.js
@@ -0,0 +1,106 @@
+const coffeeShop1 = {
+  name: 'Verve',
+  location: '8925 Melrose Avenue West Hollywood, CA 90069',
+  menu: [
+    "House Speical",
+    "Black / Americano",
+    "Latte",
+    "Mocha",
+    "Cappuccino",
+    "Espresso"
+  ],
+  figure: "./img/coffee-shop-1.jpg",
+  socialLinks: {
+    facebook: "https://www.facebook.com/vervecoffee",
+    instagram: "https://www.instagram.com/vervecoffee/",
+    twitter: "https://twitter.com/vervecoffee",
+    snapchat: "https://www.snapchat.com/"
+  }
+}
+
+const coffeeShop2 = {
+  name: 'Blue Bottle',
+  location: '2 S Park St, San Francisco, CA 94107美國',
+  menu: [
+    "Blue Speical",
+    "Caffè Latte",
+    "Caffè Mocha",
+    "Cappuccino",
+    "Espresso",
+    "Macchiato",
+    "Affogato"
+  ],
+  figure: "./img/coffee-shop-2.jpg",
+  socialLinks: {
+    facebook: "https://www.facebook.com/bluebottlecoffee",
+    instagram: "https://www.instagram.com/bluebottle/",
+    twitter: "https://twitter.com/bluebottleroast",
+    snapchat: "https://www.snapchat.com/"
+  }
+}
+
+const coffeeshop3 = {
+  name: 'Fabryka Kary',
+  location: "Ostafiya Dashkovycha St, 19, Cherkasy, Cherkas'ka oblast, Ukraine 18000",
+  menu: [
+    "Ukraine Speical",
+    "Latte",
+    "Mocha",
+    "Cappuccino",
+    "Espresso",
+    "Macchiato"
+  ],
+  figure: "./img/coffee-shop-3.jpg",
+  socialLinks: {
+    facebook: "https://www.facebook.com/fabryka.kavy.che/",
+    instagram: "https://www.instagram.com/fabryka_kavy_ck/",
+    twitter: "https://twitter.com/",
+    snapchat: "https://www.snapchat.com/"
+  }
+}
+
+const coffeeshop4 = {
+  name: 'Black Dog',
+  location: "12815 W 87th Street PKWY Lenexa, KS 66215",
+  menu: [
+    "Black Dog Speical",
+    "Black / Americano",
+    "Latte",
+    "Mocha",
+    "Cappuccino",
+    "Espresso",
+    "Macchiato"
+  ],
+  figure: "./img/coffee-shop-4.jpg",
+  socialLinks: {
+    facebook: "https://www.facebook.com/bdcoffeehouse",
+    instagram: "https://www.instagram.com/bdcoffeehouse/",
+    twitter: "https://twitter.com/bdcoffeehouse",
+    snapchat: "https://www.snapchat.com/"
+  }
+}
+
+const coffeeshop5 = {
+  name: 'CoCo Bean',
+  location: "1114 Environ Way, Chapel Hill, NC 27517",
+  menu: [
+    "Bean Speical",
+    "Black / Americano",
+    "Latte",
+    "Mocha",
+    "Flat White",
+    "Cortado",
+    "Cappuccino"
+  ],
+  figure: "./img/coffee-shop-5.jpg",
+  socialLinks: {
+    facebook: "https://www.facebook.com/cocobeancoffee",
+    instagram: "https://twitter.com/cocobeancoffee",
+    twitter: "https://www.instagram.com/cocobeancoffee/",
+    snapchat: "https://www.snapchat.com/"
+  }
+}
+
+const coffeeShops = [coffeeShop1, coffeeShop2, coffeeshop3, coffeeshop4, coffeeshop5]
+
+export default coffeeShops;
